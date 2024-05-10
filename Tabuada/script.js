fa2cd4b4e52da8f30e9c9804f28576a9fb78e6a6
Ana Claudia <anaclaudiasantanadev@gmail.com>
2024-05-10 19:10:17 -0300

    Desafio 02 - Tabuada

---
+++ Tabuada/script.js
@@ -0,0 +1,29 @@
+// Capturar o elemento de entrada 
+let input = document.getElementById('usuario')
+let submit = document.getElementById('botao')
+
+
+// Adicionar um evento de entrada ao campo 
+ submit.addEventListener('click', function (){
+    // Captura o valor inserido pelo usuário
+    var valor = parseInt(input.value)
+    
+    var tabuada = '';
+    
+       
+    for (i = 1; i <= 10; i++) {
+    
+     tabuada += valor + ' x ' + i + ' = ' + (valor * i) + '<br>';
+
+    }
+
+    document.getElementById("resposta").innerHTML = tabuada
+   
+});
+
+
+
+
+
+
+
